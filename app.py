bd7b204763cf88556e7489b321156f450a5911d7
susingh <sushant.singh@glu.com>
2019-12-05 22:32:44 +0530

    Code coverage kill

--- app.py
+++ app.py
@@ -1,24 +1,24 @@
 from flask import Flask, url_for
 app = Flask(__name__)
 
-
+#Reading version from appended file
 version = open("version", "r")
 
+#root return
 @app.route('/')
 def api_root():
     return 'Welcome'
-
+#returns build version
 @app.route('/version')
 def api_version():
     return 'Version is ' + version.read()
-
+#list article
 @app.route('/articles')
 def api_articles():
     return 'List of ' + url_for('api_articles')
-
 @app.route('/articles/<articleid>')
 def api_article(articleid):
     return 'You are reading ' + articleid
-
+#Calling main function
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
