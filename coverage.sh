52e2d4d3f6e540d94e5d55cfce358ef4a566998a
susingh <sushant.singh@glu.com>
2019-12-06 13:28:08 +0530

    removing extra files

--- coverage.sh
+++ coverage.sh
@@ -2,7 +2,7 @@
 pip install coverage
 pip install Flask
 cd $CF_VOLUME_PATH/cf-python
-ls -l
+python test_basic.py
 /usr/bin/nohup coverage run app.py &
 sleep 15
 kill -9 `ps -ef | grep app.py | awk '{print $2}'`
